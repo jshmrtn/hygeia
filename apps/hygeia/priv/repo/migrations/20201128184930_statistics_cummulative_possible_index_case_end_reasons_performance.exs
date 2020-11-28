71a2d514a19b371104ac930ababc6c2a663df246
Jonatan Männchen <jonatan@maennchen.ch>
2020-11-28 19:55:04 +0100

    Improve Statistics Performance

---
+++ apps/hygeia/priv/repo/migrations/20201128184930_statistics_cummulative_possible_index_case_end_reasons_performance.exs
@@ -0,0 +1,76 @@
+# credo:disable-for-this-file Credo.Check.Readability.Specs
+defmodule Hygeia.Repo.Migrations.StatisticsCummulativePossibleIndexCaseEndReasonsPerformance do
+  @moduledoc false
+
+  use Hygeia, :migration
+
+  # Only up on purpose
+  def up do
+    execute("""
+    DROP MATERIALIZED VIEW statistics_cumulative_possible_index_case_end_reasons;
+    """)
+
+    execute("""
+    CREATE MATERIALIZED VIEW statistics_cumulative_possible_index_case_end_reasons
+      (tenant_uuid, date, type, end_reason, count)
+      AS WITH phases AS (
+        SELECT
+          cases.tenant_uuid AS tenant_uuid,
+          cases.person_uuid AS person_uuid,
+          (phase->>'end')::date AS count_date,
+          (phase->'details'->>'type')::case_phase_possible_index_type AS count_type,
+          (phase->'details'->>'end_reason')::case_phase_possible_index_end_reason AS count_end_reason
+          FROM cases
+          CROSS JOIN UNNEST(cases.phases) AS phase
+          WHERE (
+            (phase ->> 'end')::date IS NOT NULL AND
+            phase -> 'details' ->> '__type__' = 'possible_index'
+          )
+        )
+        SELECT
+          tenants.uuid,
+          date::date,
+          type,
+          end_reason,
+          SUM(COUNT(DISTINCT phases.person_uuid)) OVER (
+            PARTITION BY type, end_reason, tenants.uuid
+            ORDER BY date::date
+          )::integer AS count
+          FROM GENERATE_SERIES(
+            LEAST((SELECT MIN(count_date) from phases), CURRENT_DATE - INTERVAL '1 year'),
+            CURRENT_DATE,
+            interval '1 day'
+          ) AS date
+          CROSS JOIN UNNEST(ENUM_RANGE(NULL::case_phase_possible_index_end_reason) || ARRAY[NULL::case_phase_possible_index_end_reason]) AS end_reason
+          CROSS JOIN UNNEST(ENUM_RANGE(NULL::case_phase_possible_index_type)) AS type
+          CROSS JOIN tenants
+          LEFT JOIN phases ON (
+            tenants.uuid = phases.tenant_uuid AND
+            date = phases.count_date AND
+            type = phases.count_type AND
+            (
+              (end_reason IS NULL AND phases.count_end_reason IS NULL) OR
+              end_reason = phases.count_end_reason
+            )
+          )
+          GROUP BY date, type, end_reason, tenants.uuid
+          ORDER BY date, type, end_reason, tenants.uuid
+    """)
+
+    create unique_index(
+             :statistics_cumulative_possible_index_case_end_reasons,
+             [
+               :tenant_uuid,
+               :date,
+               :type,
+               :end_reason
+             ],
+             name: "unique"
+           )
+
+    create index(:statistics_cumulative_possible_index_case_end_reasons, [:tenant_uuid])
+    create index(:statistics_cumulative_possible_index_case_end_reasons, [:end_reason])
+    create index(:statistics_cumulative_possible_index_case_end_reasons, [:type])
+    create index(:statistics_cumulative_possible_index_case_end_reasons, [:date])
+  end
+end
