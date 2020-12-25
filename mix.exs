ba1b25309886a28d1086128ad06f09259bca1408
Jonatan Männchen <jonatan@maennchen.ch>
2020-12-25 11:24:29 +0100

    Release v1.10.1

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Hygeia.Umbrella.MixProject do
     [
       name: "Hygeia",
       apps_path: "apps",
-      version: "1.10.1-beta.1",
+      version: "1.10.1",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
