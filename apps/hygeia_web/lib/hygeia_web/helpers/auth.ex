69c3614b1605113dacb4b62a3187a6c1b3d39bdb
Jonatan Männchen <jonatan@maennchen.ch>
2021-01-22 18:08:08 +0100

    Error Page Hotfix

--- apps/hygeia_web/lib/hygeia_web/helpers/auth.ex
+++ apps/hygeia_web/lib/hygeia_web/helpers/auth.ex
@@ -7,7 +7,10 @@ defmodule HygeiaWeb.Helpers.Auth do
   def is_logged_in?(conn_or_socket), do: get_auth(conn_or_socket) != :anonymous
 
   @spec get_auth(conn_or_socket :: Plug.Conn.t() | Phoenix.LiveView.Socket.t()) :: nil | User.t()
-  def get_auth(%Plug.Conn{} = conn), do: Plug.Conn.get_session(conn, :auth) || :anonymous
+  def get_auth(%Plug.Conn{private: %{plug_session: _}} = conn),
+    do: Plug.Conn.get_session(conn, :auth) || :anonymous
+
+  def get_auth(%Plug.Conn{} = _conn), do: :anonymous
 
   def get_auth(%Phoenix.LiveView.Socket{} = socket) do
     socket.private[:conn_session]["auth"] || socket.private[:connect_info][:session]["auth"] ||
