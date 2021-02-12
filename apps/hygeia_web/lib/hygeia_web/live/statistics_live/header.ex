bea261709575251628f523518be1305622e7c035
Jonatan Männchen <jonatan@maennchen.ch>
2021-02-12 20:45:14 +0100

    Fix Warning

--- apps/hygeia_web/lib/hygeia_web/live/statistics_live/header.ex
+++ apps/hygeia_web/lib/hygeia_web/live/statistics_live/header.ex
@@ -4,7 +4,6 @@ defmodule HygeiaWeb.StatisticsLive.Header do
   use HygeiaWeb, :surface_live_component
 
   alias HygeiaWeb.UriActiveContext
-  alias Surface.Components.Context
   alias Surface.Components.LiveRedirect
 
   prop tenant, :map, required: true
