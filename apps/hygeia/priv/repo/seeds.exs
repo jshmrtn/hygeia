3787322da747e2696759dde4eb281b2d50cd514a
Jonatan Männchen <jonatan@maennchen.ch>
2021-02-16 13:35:56 +0100

    Add Schools to Seeds

--- apps/hygeia/priv/repo/seeds.exs
+++ apps/hygeia/priv/repo/seeds.exs
@@ -159,6 +159,28 @@ tenant_ai = Enum.find(tenants, &match?(%{short_name: "AI"}, &1))
   |> Enum.reduce(Ecto.Multi.new(), &PaperTrail.Multi.insert(&2, make_ref(), &1))
   |> Repo.transaction()
 
+{:ok, _organisations} =
+  :hygeia
+  |> Application.app_dir("priv/repo/seeds/schools.csv")
+  |> File.stream!()
+  |> CSV.decode!(headers: true)
+  |> Stream.map(
+    &%{
+      name: &1["name"],
+      type: :school,
+      address: %{
+        address: &1["address"],
+        zip: &1["zip"],
+        place: &1["place"],
+        country: &1["country"],
+        subdivision: &1["subdivision"]
+      }
+    }
+  )
+  |> Stream.map(&change_new_organisation/1)
+  |> Enum.reduce(Ecto.Multi.new(), &PaperTrail.Multi.insert(&2, make_ref(), &1))
+  |> Repo.transaction()
+
 if System.get_env("LOAD_SAMPLE_DATA", "false") in ["1", "true"] do
   {:ok, _sedex_export_sg} =
     create_sedex_export(tenant_sg, %{
