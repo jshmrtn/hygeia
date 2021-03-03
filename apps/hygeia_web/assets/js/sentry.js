41622972f164c7433f137add99cde7c57d494635
Jonatan Männchen <jonatan@maennchen.ch>
2021-03-03 21:07:37 +0000

    Enable Sentry APM

--- apps/hygeia_web/assets/js/sentry.js
+++ apps/hygeia_web/assets/js/sentry.js
@@ -3,9 +3,14 @@ import {
   showReportDialog as sentryShowReportDialog,
   setUser as sentrySetUser,
 } from "@sentry/browser";
+import { Integrations as TracingIntegrations } from "@sentry/tracing";
 
 export function init(dataset) {
-  sentryInit({ dsn: dataset.sentryDsn });
+  sentryInit({
+    dsn: dataset.sentryDsn,
+    integrations: [new TracingIntegrations.BrowserTracing()],
+    tracesSampleRate: 0.1
+  });
   sentrySetUser(JSON.parse(dataset.sentryUser));
   document.addEventListener(
     "DOMContentLoaded",
