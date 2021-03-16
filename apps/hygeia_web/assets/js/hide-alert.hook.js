5e5ab6ee781efb6f132683c374374782b179799d
Leo Zurbriggen <zurbriggen@joshmartin.ch>
2021-03-16 14:49:25 +0100

    finalize

--- apps/hygeia_web/assets/js/hide-alert.hook.js
+++ apps/hygeia_web/assets/js/hide-alert.hook.js
@@ -5,23 +5,29 @@ const Hook = {
     return this.el.dataset.alertId;
   },
   mounted() {
+    this.target = this.el.dataset.phxComponent;
+    this.clickCallbacks = {};
+
     const storageData = localStorage.getItem(storageKey);
     this.hiddenAlerts = [];
-    try {
-      if (storageData) {
+    if (storageData) {
+      try {
         this.hiddenAlerts = JSON.parse(storageData);
+      } catch {
+        console.warn("Failed to load hidden alerts from localStorage.");
       }
-    } catch {
-      console.warn("Failed to load hidden alerts from localStorage.");
     }
-    this.pushEvent("hide_alerts", { alertIds: this.hiddenAlerts });
-
+    this.pushEventTo(this.target, "hide_alerts", { alertIds: this.hiddenAlerts });
+    this.hideAlert = (event) => {
+      this.hiddenAlerts.push(event.target.dataset.alertId);
+      localStorage.setItem(storageKey, JSON.stringify(this.hiddenAlerts));
+      this.pushEventTo(this.target, "hide_alerts", { alertIds: this.hiddenAlerts });
+    };
+  },
+  updated() {
     this.el.querySelectorAll("button").forEach((b) => {
-      b.addEventListener("click", () => {
-        this.hiddenAlerts.push(b.dataset.alertId);
-        localStorage.setItem(storageKey, JSON.stringify(this.hiddenAlerts));
-        this.pushEvent("hide_alerts", { alertIds: this.hiddenAlerts });
-      });
+      b.removeEventListener("click", this.hideAlert);
+      b.addEventListener("click", this.hideAlert);
     });
   },
 };
---
+++ apps/hygeia_web/lib/hygeia_web/live/system_messages_banner.ex
@@ -0,0 +1,19 @@
+defmodule HygeiaWeb.SystemMessagesBanner do
+  @moduledoc false
+
+  use HygeiaWeb, :surface_live_component
+
+  data hidden_message_ids, :list, default: nil
+
+  @impl Phoenix.LiveComponent
+  def handle_event("hide_alerts", %{"alertIds" => hidden_message_ids}, socket) do
+    {:noreply, assign(socket, hidden_message_ids: hidden_message_ids)}
+  end
+
+  defp filtered_system_messages(auth, hidden_message_ids) do
+    Enum.filter(
+      Hygeia.SystemMessageContext.list_active_system_messages(auth),
+      fn {id, _msg} -> not (id in hidden_message_ids) end
+    )
+  end
+end
