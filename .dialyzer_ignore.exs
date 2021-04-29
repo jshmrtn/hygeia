291e2436f1a28c0d1eb225b6fff2316be13c84af
Jonatan Männchen <jonatan@maennchen.ch>
2021-04-29 16:22:37 +0200

    Update Deps

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,8 +1,4 @@
 [
    # Phoenix Bug
-   {"lib/phoenix/router.ex", :pattern_match, 402},
-   # Phoenix Live View Bug
-   # https://github.com/phoenixframework/phoenix_live_view/pull/1419
-   {"lib/hygeia_web/live_view.ex", :pattern_match, 97},
-   {"lib/hygeia_web/live_view.ex", :pattern_match_cov, 98}
+   {"lib/phoenix/router.ex", :pattern_match, 402}
 ]
