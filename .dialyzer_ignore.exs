d19b03fd839ccf2ad95e5b904d2b1bb1218bc6c8
Jonatan Männchen <jonatan@maennchen.ch>
2021-07-23 12:05:07 +0100

    Ignore Dialyzer Warning (elixir-plug/plug#1042)

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,4 +1,13 @@
 [
-   # Phoenix Bug
-   {"lib/phoenix/router.ex", :pattern_match, 402}
+   # Absinthe Plug Bug
+   {"lib/phoenix/router.ex", :pattern_match, 402},
+   # TODO: Remove when https://github.com/elixir-plug/plug/issues/1042 is solved
+   {"lib/hygeia_web/plug/setup_versioning.ex", :callback_type_mismatch},
+   {"lib/hygeia_web/plug/setup_versioning.ex", :callback_arg_type_mismatch},
+   {"lib/hygeia_web/plug/require_authentication.ex", :callback_type_mismatch},
+   {"lib/hygeia_web/plug/require_authentication.ex", :callback_arg_type_mismatch},
+   {"lib/hygeia_web/plug/has_role.ex", :callback_type_mismatch},
+   {"lib/hygeia_web/plug/has_role.ex", :callback_arg_type_mismatch},
+   {"lib/hygeia_web/plug/check_and_refresh_authentication.ex", :callback_type_mismatch},
+   {"lib/hygeia_web/plug/check_and_refresh_authentication.ex", :callback_arg_type_mismatch},
 ]
