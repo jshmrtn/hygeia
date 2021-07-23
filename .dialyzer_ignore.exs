7c3107ce9deb8cb7eae07359d3c675c065864434
Jonatan Männchen <jonatan@maennchen.ch>
2021-07-23 12:24:42 +0100

    Remove wrongly added dialyzer ignores

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,13 +1,4 @@
 [
    # Absinthe Plug Bug
-   {"lib/phoenix/router.ex", :pattern_match, 402},
-   # TODO: Remove when https://github.com/elixir-plug/plug/issues/1042 is solved
-   {"lib/hygeia_web/plug/setup_versioning.ex", :callback_type_mismatch},
-   {"lib/hygeia_web/plug/setup_versioning.ex", :callback_arg_type_mismatch},
-   {"lib/hygeia_web/plug/require_authentication.ex", :callback_type_mismatch},
-   {"lib/hygeia_web/plug/require_authentication.ex", :callback_arg_type_mismatch},
-   {"lib/hygeia_web/plug/has_role.ex", :callback_type_mismatch},
-   {"lib/hygeia_web/plug/has_role.ex", :callback_arg_type_mismatch},
-   {"lib/hygeia_web/plug/check_and_refresh_authentication.ex", :callback_type_mismatch},
-   {"lib/hygeia_web/plug/check_and_refresh_authentication.ex", :callback_arg_type_mismatch},
+   {"lib/phoenix/router.ex", :pattern_match, 402}
 ]
