81e3670c4329f6d7cc6306275a7d19487a629116
Jonatan Männchen <jonatan@maennchen.ch>
2021-08-27 10:16:08 +0200

    Inbox set end reason of last phase (#453) (#462)

--- apps/hygeia/test/hygeia/import_context/planner/generator/ism_2021_06_11_test_test.exs
+++ apps/hygeia/test/hygeia/import_context/planner/generator/ism_2021_06_11_test_test.exs
@@ -1,4 +1,5 @@
 # credo:disable-for-this-file Credo.Check.Readability.ModuleNames
+# credo:disable-for-this-file Credo.Check.Design.DuplicatedCode
 defmodule Hygeia.ImportContext.Planner.Generator.ISM_2021_06_11_TestTest do
   @moduledoc false
 
@@ -39,9 +40,11 @@ defmodule Hygeia.ImportContext.Planner.Generator.ISM_2021_06_11_TestTest do
   end
 
   test "runs correct plan for new case", %{
-    import: %Import{rows: [row | _others]},
+    import: %Import{rows: rows},
     tenant_sg: %Tenant{uuid: tenant_sg_uuid}
   } do
+    row = Enum.find(rows, &(&1.data["Meldung ID"] == 1_794_060))
+
     {true, action_plan_suggestion} = Planner.generate_action_plan_suggestion(row)
 
     action_plan = Enum.map(action_plan_suggestion, &elem(&1, 1))
@@ -104,6 +107,125 @@ defmodule Hygeia.ImportContext.Planner.Generator.ISM_2021_06_11_TestTest do
     assert %Person{
              uuid: ^person_uuid,
              external_references: [%ExternalReference{type: :ism_patient, value: "1561000"}],
+             last_name: "Licht",
+             first_name: "Laura",
+             birth_date: ~D[1943-09-30],
+             sex: :female,
+             address: %Address{
+               address: "Musterstraße 10",
+               zip: "9443",
+               place: "Widnau",
+               country: "CH"
+             },
+             tenant_uuid: ^tenant_sg_uuid
+           } = person
+  end
+
+  test "runs correct plan for existing case", %{
+    import: %Import{rows: rows},
+    tenant_sg: %Tenant{uuid: tenant_sg_uuid} = tenant_sg
+  } do
+    row = Enum.find(rows, &(&1.data["Meldung ID"] == 1_794_060))
+
+    person =
+      person_fixture(tenant_sg, %{
+        last_name: "Muster",
+        first_name: "Max",
+        external_references: [%{type: :ism_patient, value: "1673735"}]
+      })
+
+    case =
+      case_fixture(
+        person,
+        user_fixture(%{iam_sub: Ecto.UUID.generate()}),
+        user_fixture(%{iam_sub: Ecto.UUID.generate()}),
+        %{
+          external_references: [
+            %{type: :ism_case, value: "2182953"},
+            %{type: :ism_report, value: "1794060"}
+          ],
+          phases: [
+            %{
+              details: %{
+                __type__: :possible_index,
+                type: :contact_person
+              },
+              start: ~D[2020-10-10],
+              end: ~D[2020-10-11],
+              quarantine_order: true
+            }
+          ],
+          tests: []
+        }
+      )
+
+    case_uuid = case.uuid
+
+    {true, action_plan_suggestion} = Planner.generate_action_plan_suggestion(row)
+
+    action_plan = Enum.map(action_plan_suggestion, &elem(&1, 1))
+
+    assert {:ok,
+            %{
+              row: row,
+              case: case,
+              person: person
+            }} = Planner.execute(action_plan, row)
+
+    assert %Row{
+             case_uuid: ^case_uuid,
+             status: :resolved
+           } = row
+
+    assert %Case{
+             uuid: ^case_uuid,
+             person_uuid: person_uuid,
+             external_references: [
+               %ExternalReference{type: :ism_case, value: "2182953"},
+               %ExternalReference{type: :ism_report, value: "1794060"}
+             ],
+             phases: [
+               %Case.Phase{details: %Case.Phase.PossibleIndex{end_reason: :converted_to_index}},
+               %Case.Phase{details: %Case.Phase.Index{}}
+             ],
+             tenant_uuid: ^tenant_sg_uuid,
+             tests: [
+               %Test{
+                 kind: :pcr,
+                 laboratory_reported_at: ~D[2021-03-24],
+                 reference: "21 3240 0755",
+                 reporting_unit: %Entity{
+                   address: %Address{
+                     address: "Lagerstrasse 30",
+                     country: "CH",
+                     place: "Buchs SG",
+                     zip: "9470"
+                   },
+                   division: "Buchs",
+                   name: "Labormedizinisches Zentrum Dr. Risch"
+                 },
+                 result: :inconclusive,
+                 sponsor: %Hygeia.CaseContext.Entity{
+                   address: %Hygeia.CaseContext.Address{
+                     address: "Chnoblisbüel 1",
+                     country: "CH",
+                     place: "Walenstadtberg",
+                     subdivision: nil,
+                     zip: "8881"
+                   },
+                   division: "Rehazentrum Walenstadtberg",
+                   name: "Kliniken Valens",
+                   person_first_name: nil,
+                   person_last_name: nil
+                 },
+                 tested_at: ~D[2021-03-24]
+               }
+             ]
+           } = case
+
+    assert %Person{
+             uuid: ^person_uuid,
+             external_references: [%ExternalReference{type: :ism_patient, value: "1673735"}],
              last_name: "Licht",
              first_name: "Laura",
              birth_date: ~D[1943-09-30],
