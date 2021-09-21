e8c6c5bd8774dbe61fa2397e1386d06f9f233530
Jonatan Männchen <jonatan@maennchen.ch>
2021-09-21 09:58:21 +0100

    Replace Hygeia.LiveView with Phoenix LiveView Hooks (#636)

--- apps/hygeia_web/.formatter.exs
+++ apps/hygeia_web/.formatter.exs
@@ -1,5 +1,5 @@
 [
-  import_deps: [:phoenix, :surface, :ecto],
+  import_deps: [:phoenix, :phoenix_live_view, :surface, :ecto],
   inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
   surface_inputs: ["lib/**/*.{ex,sface}"]
 ]
