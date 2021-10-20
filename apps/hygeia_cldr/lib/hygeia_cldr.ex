15dc80c445edc87c2b49bb628272884690d4e4df
Jonatan Männchen <jonatan@maennchen.ch>
2021-10-20 15:29:52 +0200

    cldr data dir fix

--- apps/hygeia_cldr/lib/hygeia_cldr.ex
+++ apps/hygeia_cldr/lib/hygeia_cldr.ex
@@ -5,7 +5,7 @@ defmodule HygeiaCldr do
     default_locale: "en-CH",
     locales: ["en", "en-CH", "de", "de-CH", "fr", "fr-CH", "it", "it-CH"],
     gettext: HygeiaGettext,
-    data_dir: Application.app_dir(:hygeia_cldr, "priv/cldr"),
+    data_dir: "./priv/cldr",
     otp_app: :hygeia_cldr,
     providers: [Cldr.Number, Cldr.List, Cldr.Calendar, Cldr.DateTime, Cldr.Unit, Cldr.Language],
     generate_docs: true,
