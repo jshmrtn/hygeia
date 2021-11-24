b32a19c359dde0ed3919979e2739fbcc5c68848f
Jonatan Männchen <jonatan@maennchen.ch>
2021-11-24 12:41:48 +0000

    Improve Sentry Tracing

--- assets/js/sentry.js
+++ assets/js/sentry.js
@@ -5,11 +5,27 @@ import {
 } from "@sentry/browser";
 import { Integrations as TracingIntegrations } from "@sentry/tracing";
 
+function currentUrlWithoutParameters() {
+  return location.pathname
+    .replace(
+      /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/gi,
+      "<uuid>"
+    )
+    .replace(/\d{4}-\d{2}-\d{2}/g, "<date>");
+}
+
 export function init(dataset) {
   sentryInit({
     dsn: dataset.sentryDsn,
-    integrations: [new TracingIntegrations.BrowserTracing()],
-    tracesSampleRate: 0.1
+    integrations: [
+      new TracingIntegrations.BrowserTracing({
+        beforeNavigate: (context) => ({
+          ...context,
+          name: currentUrlWithoutParameters(),
+        }),
+      }),
+    ],
+    tracesSampleRate: 0.1,
   });
   sentrySetUser(JSON.parse(dataset.sentryUser));
   document.addEventListener(
