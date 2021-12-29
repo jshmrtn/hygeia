402f05637e12d8855a621b58331f06332c4be098
Antonio Toncetti <antonio.toncetti@gmail.com>
2021-12-29 14:33:08 +0100

    Release v1.38.0-beta.1

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Hygeia.MixProject do
     [
       app: :hygeia,
       name: "Hygeia",
-      version: "1.37.0",
+      version: "1.38.0-beta.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
