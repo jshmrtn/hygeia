6f504f79c9ad637b00726d91a3252998a6c408e8
atoncetti <antonio.toncetti@gmail.com>
2022-01-11 00:00:12 +0100

    Added migration.

--- priv/repo/migrations/20220106132952_create_premature_release_disabled_reason.exs
+++ priv/repo/migrations/20220106132952_create_premature_release_disabled_reason.exs
@@ -20,11 +20,38 @@ defmodule Hygeia.Repo.Migrations.CreatePrematureReleaseDisabledReason do
     execute(
       """
       UPDATE cases update_case
-      SET phases = update_case.phases
+      SET phases = replace_case.new_phases
+      FROM (
+        SELECT
+          cases.uuid AS case_uuid,
+          ARRAY_AGG(
+            JSONB_BUILD_OBJECT('premature_release_permission', 'true', 'premature_release_disabled_reason', NULL, 'premature_release_disabled_reason_other', NULL) ||
+            phase
+          ) AS new_phases
+          FROM cases
+          CROSS JOIN
+          UNNEST(cases.phases)
+          AS phase
+        GROUP BY cases.uuid
+      ) replace_case
+      WHERE replace_case.case_uuid = update_case.uuid;
       """,
       """
       UPDATE cases update_case
-      SET phases = update_case.phases;
+      SET phases = replace_case.new_phases
+      FROM (
+        SELECT
+          cases.uuid AS case_uuid,
+          ARRAY_AGG(
+            phase - 'premature_release_permission' - 'premature_release_disabled_reason' - 'premature_release_disabled_reason_other'
+          ) AS new_phases
+          FROM cases
+          CROSS JOIN
+          UNNEST(cases.phases)
+          AS phase
+        GROUP BY cases.uuid
+      ) replace_case
+      WHERE replace_case.case_uuid = update_case.uuid;
       """
     )
 
