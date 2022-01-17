377ed08ce04ea30f0dbd536b18deede23c702063
Jonatan Männchen <jonatan@maennchen.ch>
2022-01-17 22:39:24 +0100

    Release v1.41.0-beta.5

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Hygeia.MixProject do
     [
       app: :hygeia,
       name: "Hygeia",
-      version: "1.41.0-beta.4",
+      version: "1.41.0-beta.5",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
