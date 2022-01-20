87f38c638cd68c900ccb7a657623309948d53a48
Jonatan Männchen <jonatan@maennchen.ch>
2022-01-20 12:15:14 +0100

    Increse Refresh Time for Vaccination Validity Calculation

--- config/config.exs
+++ config/config.exs
@@ -80,7 +80,7 @@ config :hygeia, Hygeia.Jobs.SendCaseClosedEmail,
 config :hygeia, Hygeia.AutoTracingContext.AutoTracingCommunication,
   url_generator: HygeiaWeb.AutoTracingCommunicationUrlGenerator
 
-config :hygeia, Hygeia.Jobs.Supervisor, vaccination_refresh_interval_ms: :timer.minutes(5)
+config :hygeia, Hygeia.Jobs.Supervisor, vaccination_refresh_interval_ms: :timer.minutes(15)
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
