50d707ebe7676ab657636601a2ceb38582cfadc2
Jonatan Männchen <jonatan@maennchen.ch>
2022-03-03 10:25:17 +0100

    Remove Dialyzer Ignore for Fixed CldrLanguages Issue

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,1 @@
-[
-  # TODO: Remove when the following issue is fixed:
-  # https://github.com/elixir-cldr/cldr/issues/166#issuecomment-1056881807
-  {"lib/hygeia_cldr.ex", :invalid_contract, 1}
-]
+[]
