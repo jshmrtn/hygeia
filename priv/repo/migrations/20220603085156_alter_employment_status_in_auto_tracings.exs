3164a53b64d12810af2c1b294102b9495347e676
Antonio Toncetti <antonio.toncetti@gmail.com>
2022-06-03 13:47:53 +0200

    Simplify migration.

--- priv/repo/migrations/20220603085156_alter_employment_status_in_auto_tracings.exs
+++ priv/repo/migrations/20220603085156_alter_employment_status_in_auto_tracings.exs
@@ -11,44 +11,20 @@ defmodule Hygeia.Repo.Migrations.AlterEmploymentStatusInAutoTracings do
       :ok = run_authentication(repo(), origin: :migration, originator: :noone)
     end)
 
-    alter table(:auto_tracings) do
-      add :employed_tmp, :boolean
-    end
-
-    execute(
-      """
-      UPDATE auto_tracings
-      SET employed_tmp = employed;
-      """,
-      &noop/0
-    )
-
-    alter table(:auto_tracings) do
-      remove :employed
-    end
-
     EmploymentStatus.create_type()
 
-    alter table(:auto_tracings) do
-      add :employed, EmploymentStatus.type()
-    end
-
-    execute(
-      """
-      UPDATE auto_tracings at
-      SET employed =
-        CASE
-          WHEN at.employed_tmp IS TRUE THEN 'yes'::employment_status
-          WHEN at.employed_tmp IS FALSE THEN 'no'::employment_status
-          ELSE NULL
-        END;
-      """,
-      &noop/0
-    )
-
-    alter table(:auto_tracings) do
-      remove :employed_tmp
-    end
+    execute("""
+    ALTER
+    table auto_tracings
+    ALTER
+      employed
+      SET DATA TYPE #{EmploymentStatus.type()}
+      USING CASE
+      WHEN employed IS TRUE THEN 'yes'::employment_status
+      WHEN employed IS FALSE THEN 'no'::employment_status
+      ELSE NULL
+    END;
+    """)
   end
 
   def down do
@@ -56,42 +32,18 @@ defmodule Hygeia.Repo.Migrations.AlterEmploymentStatusInAutoTracings do
       :ok = run_authentication(repo(), origin: :migration, originator: :noone)
     end)
 
-    alter table(:auto_tracings) do
-      add :employed_tmp, EmploymentStatus.type()
-    end
-
-    execute(
-      """
-      UPDATE auto_tracings
-      SET employed_tmp = employed;
-      """,
-      &noop/0
-    )
-
-    alter table(:auto_tracings) do
-      remove :employed
-    end
-
-    alter table(:auto_tracings) do
-      add :employed, :boolean
-    end
-
-    execute(
-      """
-      UPDATE auto_tracings at
-      SET employed =
-        CASE
-          WHEN at.employed_tmp = 'yes' THEN TRUE
-          WHEN at.employed_tmp = 'no' THEN FALSE
-          ELSE NULL
-        END;
-      """,
-      &noop/0
-    )
-
-    alter table(:auto_tracings) do
-      remove :employed_tmp
-    end
+    execute("""
+    ALTER
+    table auto_tracings
+    ALTER
+      employed
+      SET DATA TYPE BOOL
+      USING CASE
+      WHEN employed = 'yes' THEN TRUE
+      WHEN employed = 'no' THEN FALSE
+      ELSE NULL
+    END;
+    """)
 
     EmploymentStatus.drop_type()
   end
