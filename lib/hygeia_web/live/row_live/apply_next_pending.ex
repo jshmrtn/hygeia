578a0bfbe1a310d39d964393fd60fcdd68cc442d
Antonio Toncetti <antonio.toncetti@gmail.com>
2022-06-09 16:24:38 +0200

    Iterate through inbox rows in an orderly manner with rows ordered by uuid asc.

--- lib/hygeia_web/live/row_live/apply_next_pending.ex
+++ lib/hygeia_web/live/row_live/apply_next_pending.ex
@@ -14,11 +14,12 @@ defmodule HygeiaWeb.RowLive.ApplyNextPending do
      |> ImportContext.get_import!()
      |> Repo.preload(pending_rows: [])
      |> case do
-       %Import{pending_rows: [next_row | _others]} ->
-         push_redirect(socket, to: Routes.row_apply_path(socket, :apply, next_row))
-
        %Import{pending_rows: []} = import ->
          push_redirect(socket, to: Routes.import_show_path(socket, :show, import))
+
+       %Import{pending_rows: rows} ->
+         [next_row | _other] = Enum.sort_by(rows, & &1.uuid, :asc)
+         push_redirect(socket, to: Routes.row_apply_path(socket, :apply, next_row))
      end}
   end
 
