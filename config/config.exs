f8d66b70e9ada10f064ec2224ea33aa038fdf759
Jonatan Männchen <jonatan@maennchen.ch>
2023-03-09 17:15:45 +0100

    Lower Anonymization / Pruning Threshold for platform pause

--- config/config.exs
+++ config/config.exs
@@ -92,8 +92,10 @@ config :hygeia, Hygeia.AutoTracingContext.AutoTracingCommunication,
 config :ex_cldr,
   default_backend: HygeiaCldr
 
-config :hygeia, Hygeia.Jobs.DataPruning, threshold: {2, "year"}
-config :hygeia, Hygeia.Jobs.Anonymization, threshold: {2, "year"}
+# Temporarily lower threshold from 2 years to 1 month
+# TODO: Change back before going into full production mode again
+config :hygeia, Hygeia.Jobs.DataPruning, threshold: {1, "month"}
+config :hygeia, Hygeia.Jobs.Anonymization, threshold: {1, "month"}
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
